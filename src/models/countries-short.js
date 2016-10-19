489ff2b1bf708214d582ddc351cafa6d96b634ac
smikodanic <smikodanic@gmail.com>
2016-10-19 20:43:26 +0200

    short list

--- src/models/countries-short.js
+++ src/models/countries-short.js
@@ -1,10 +1,17 @@
-module.exports = function () {
-    'use strict';
-    return [
-        {
-        "name": "Afghanistan",
-        "dial_code": "+93",
-        "code": "AF"
-        }
-    ]
-};
+module.exports = [
+    {
+    "name": "Afghanistan",
+    "dial_code": "+93",
+    "code": "AF"
+    },
+    {
+    "name": "Aland Islands",
+    "dial_code": "+358",
+    "code": "AX"
+    },
+    {
+    "name": "Albania",
+    "dial_code": "+355",
+    "code": "AL"
+    }
+];
